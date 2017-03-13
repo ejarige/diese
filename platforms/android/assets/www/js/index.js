47e314cbdc28bae476983fd062f1ec3b3753a389
Jarrige <eric.jarrige@live.fr>
2017-03-13 20:00:48 +0100

    ajout onsen , webservice

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,46 +1,121 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+var DIESE_SERVICE = 'http://diese.pe.hu/services/';
+$(function() {
+    // MERCI ET TARPLU
+    $('#back-home').on('click', function(){
+        $('#ty-and-bye').addClass('hidden');
+        $('ons-carousel-item').removeClass('hidden');
+    });
+    // CONNEXION
+    $('#sign-in').on('click', function(){
+        alert('Bientôt disponible');
+    });
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+    // INSCRIPTION
+    $('#formulaire-inscription').on('submit', function(){
+        // nettoyage formulaire
+        var form = $('#form-submit');
+        form.prop('disabled', true);
+        $('.infobox').text('');
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+        // verification des champs obligatoires
+        var empty = false;
+        $('.required').each(function(){
+            var thisEmpty = !$(this).val().length;
+            if(thisEmpty)
+                $('#info-'+$(this).attr('id')).text('Ce champ est obligatoire !');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+           empty = empty || thisEmpty;
+        });
 
-        console.log('Received Event: ' + id);
-    }
-};
+        if(empty) {
+            form.prop('disabled', false);
+            return false;
+        }
 
-app.initialize();
+        // verification login
+        var login      = $('#pseudo').val();
+        var loginError = $('#info-pseudo');
+        var loginRegex = /^[a-zA-Z0-9_]{3,16}$/;
+
+        if(!loginRegex.test(login)){
+            loginError.text('Doit être composé de 3 à 16 caractères alpha-numériques');
+            form.prop('disabled', false);
+            return false;
+        }
+
+        // verification email
+        var email      = $('#email').val();
+        var emailError = $('#info-email');
+        var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if(!emailRegex.test(email)){
+            emailError.text('Mail incorrect');
+            form.prop('disabled', false);
+            return false;
+        }
+
+        // ouverture fenetre chargement
+        var loading = $('#creating-account');
+        loading.show();
+
+        // callback création réussie
+        var onLoad = function(e){
+            loading.hide();
+            form.prop('disabled', false);
+            $('ons-carousel-item').addClass('hidden');
+            $('#ty-and-bye').removeClass('hidden');
+        };
+
+        // callback erreur
+        var onError = function(e){
+            console.log(e);
+            if(e.status == 401){
+                var data = $.parseJSON(e.responseText);
+                loading.hide();
+
+                for(var i in data){
+                    if(data[i].email == $('#email').val())
+                        emailError.text('Un compte avec ce mail existe déjà.');
+
+                    if(data[i].login == $('#pseudo').val())
+                        loginError.text('Ce pseudo existe déjà.');
+                }
+                form.prop('disabled', false);
+            } else {
+                loading.hide();
+                alert('une erreur est survenue');
+                console.log(e);
+                form.prop('disabled', false);
+            }
+        };
+
+        // appel service inscription
+        askDiese(
+            'create/account',
+            {
+                login    : login,
+                email    : email,
+                password : sha1($('#pass').val()),
+                prenom   : $('#firstname').val()
+            },
+            onLoad,
+            onError
+        );
+        return false;
+    });
+
+    // Enlève messages d'erreur a la modification
+    $('.required').on('change', function(){
+       $('#info-'+$(this).attr('id')).text('');
+    });
+});
+
+function askDiese(url, values, onSuccess, onError){
+    $.ajax({
+      type      : 'POST',
+      url       : DIESE_SERVICE+url+'.php',
+      data      : values,
+      success   : onSuccess,
+      error     : onError
+    });
+}
--- www/js/index.js
+++ www/js/index.js
@@ -1,45 +1,119 @@
+var DIESE_SERVICE = 'http://diese.pe.hu/services/';
 $(function() {
-    // CARDS NAV
-    var CARDS_MAX = 2;
-    
-    $(document).hammer().on("swiperight", function() {
-        var prevCard = parseInt($('.active').attr('id').split('-')[1])-1;
-        if(prevCard > 0){
-            var prevCardId = '#page-'+prevCard;
-            $('.card').removeClass('active');
-            $(prevCardId).addClass('active');
-        }
+    // MERCI ET TARPLU
+    $('#back-home').on('click', function(){
+        $('#ty-and-bye').addClass('hidden');
+        $('ons-carousel-item').removeClass('hidden');
     });
-    
-    $(document).hammer().on("swipeleft", function() {
-        var nextCard = parseInt($('.active').attr('id').split('-')[1])+1;
-        if(nextCard <= CARDS_MAX){
-            var nextCardId = '#page-'+nextCard;
-            $('.card').removeClass('active');
-            $(nextCardId).addClass('active');
-        }
+    // CONNEXION
+    $('#sign-in').on('click', function(){
+        alert('Bientôt disponible');
     });
-    
+
     // INSCRIPTION
     $('#formulaire-inscription').on('submit', function(){
-        inscription({
-            login    : $('#pseudo').val(),
-            email    : $('#email').val(),
-            password : $('#pass').val(),
-            prenom   : $('#firstname').val()
-        }, function(){
-            alert('Compte créé');
-        }, function(){
-            alert('Erreur');
+        // nettoyage formulaire
+        var form = $('#form-submit');
+        form.prop('disabled', true);
+        $('.infobox').text('');
+
+        // verification des champs obligatoires
+        var empty = false;
+        $('.required').each(function(){
+            var thisEmpty = !$(this).val().length;
+            if(thisEmpty)
+                $('#info-'+$(this).attr('id')).text('Ce champ est obligatoire !');
+
+           empty = empty || thisEmpty;
         });
+
+        if(empty) {
+            form.prop('disabled', false);
+            return false;
+        }
+
+        // verification login
+        var login      = $('#pseudo').val();
+        var loginError = $('#info-pseudo');
+        var loginRegex = /^[a-zA-Z0-9_]{3,16}$/;
+
+        if(!loginRegex.test(login)){
+            loginError.text('Doit être composé de 3 à 16 caractères alpha-numériques');
+            form.prop('disabled', false);
+            return false;
+        }
+
+        // verification email
+        var email      = $('#email').val();
+        var emailError = $('#info-email');
+        var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if(!emailRegex.test(email)){
+            emailError.text('Mail incorrect');
+            form.prop('disabled', false);
+            return false;
+        }
+
+        // ouverture fenetre chargement
+        var loading = $('#creating-account');
+        loading.show();
+
+        // callback création réussie
+        var onLoad = function(e){
+            loading.hide();
+            form.prop('disabled', false);
+            $('ons-carousel-item').addClass('hidden');
+            $('#ty-and-bye').removeClass('hidden');
+        };
+
+        // callback erreur
+        var onError = function(e){
+            console.log(e);
+            if(e.status == 401){
+                var data = $.parseJSON(e.responseText);
+                loading.hide();
+
+                for(var i in data){
+                    if(data[i].email == $('#email').val())
+                        emailError.text('Un compte avec ce mail existe déjà.');
+
+                    if(data[i].login == $('#pseudo').val())
+                        loginError.text('Ce pseudo existe déjà.');
+                }
+                form.prop('disabled', false);
+            } else {
+                loading.hide();
+                alert('une erreur est survenue');
+                console.log(e);
+                form.prop('disabled', false);
+            }
+        };
+
+        // appel service inscription
+        askDiese(
+            'create/account',
+            {
+                login    : login,
+                email    : email,
+                password : sha1($('#pass').val()),
+                prenom   : $('#firstname').val()
+            },
+            onLoad,
+            onError
+        );
         return false;
+    });
+
+    // Enlève messages d'erreur a la modification
+    $('.required').on('change', function(){
+       $('#info-'+$(this).attr('id')).text('');
     });
 });
 
-function inscription(values, onSuccess, onError){
+function askDiese(url, values, onSuccess, onError){
     $.ajax({
       type      : 'POST',
-      url       : 'http://diese.pe.hu/services/create/account.php',
+      url       : DIESE_SERVICE+url+'.php',
       data      : values,
       success   : onSuccess,
       error     : onError
