8e6b63a66862a5c39b9461f20612056ac5c9f57c
Xotia <gaillardk621@gmail.com>
2017-04-09 16:00:37 +0200

    ajout de favoris concerts ajout de favoris artistes ajout de messagerie tentative de profil.js

--- www/js/profil.js
+++ www/js/profil.js
@@ -2,21 +2,35 @@
  * Created by kgaillar on 29/03/2017.
  */
 $(function(){
-   RecupDataUser();
+    RecupDataUser();
 })
 function RecupDataUser(){
-    var onLoad=function(e){
-        console.log(e);
+
+    var onLoad = function(e){
+        var data = $.parseJSON(e);
+        console.log(data, (data.length != 0));
+        if(data && (data.length != 0)){
+            afficherInfo(data);
+        } else {
+            alert('aucun résultat');
+        }
     };
-    var onError=function(e){
-        console.log(e);
+
+    var onError = function(e){
+        alert('Erreur '+ e.status);
     };
+
     askDiese(
         "get/user",
-    {
-        user_id:28
-    },
-    onLoad,
-        onError
-    )
+        {
+            user_id:28
+        },
+        onLoad,
+        onError);
+}
+
+function afficherInfo(data){
+    var infoLogin = '<p>'+data.login+'</p>';
+
+    $('#prenom_age_ville').append(infoLogin);
 }
