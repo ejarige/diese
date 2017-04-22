72641bfd7080dc126098ec244c3b10b3b8dcc3b3
Jarrige <eric.jarrige@live.fr>
2017-04-22 17:01:26 +0200

    fixed profile modification

--- www/js/profil.js
+++ www/js/profil.js
@@ -1,6 +1,8 @@
 /**
  * Created by kgaillar on 29/03/2017.
  */
+var PROFIL_DATA;
+
 $(function(){
     recupDataUser();
     addEditListener();
@@ -12,8 +14,8 @@ function recupDataUser(){
         var data = $.parseJSON(e);
         console.log(data, (data.length != 0));
         if(data && (data.length != 0)){
+            PROFIL_DATA = data;
             afficherInfo(data);
-            preFillForm(data);
         } else {
             alert('aucun résultat');
         }
@@ -50,6 +52,78 @@ function preFillForm(data){
 
 function addEditListener(){
     var openSettings = function(){
+        var modal = '<div id="edit-profile">'
+            +'<h1>Modifier mon profil</h1>'
+            +'<form id="edit-form">'
+                +'<label id="edit-avatar-label" for="edit-avatar"></label>'
+                +'<input id="edit-avatar" type="file">'
+                +'<label for="edit-name">Prénom</label>'
+                +'<input type="text" id="edit-name" maxlength="16"><br>'
+                +'<label for="edit-age">Age</label>'
+                +'<input type="number" id="edit-age" min="18" step="1" max="99"><br>'
+                +'<label for="edit-location">Ville</label>'
+                +'<input type="text" id="edit-location" maxlength="32">'
+                +'<div id="edit-actions">'
+                    +'<input type="submit" value="Valider">'
+                    +'<input type="button" id="cancel" value="Annuler">'
+                +'</div>'
+            +'</form>'
+            +'</div>';
+
+        $('body').append(modal);
+
+        preFillForm(PROFIL_DATA);
+
+        $('#cancel').on('click', function(){
+            closeSettings();
+        });
+
+        $('#edit-avatar').on('change', function(){
+            if (this.files && this.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function (e) {
+                    $('#edit-avatar-label').css({backgroundImage:'url('+ e.target.result+')'});
+                };
+
+                reader.readAsDataURL(this.files[0]);
+            }
+        });
+
+        $('#edit-form').on('submit', function(){
+            openLoading('Modification en cours');
+
+            var form = new FormData();
+
+            form.append('age',      $('#edit-age').val());
+            form.append('prenom',   $('#edit-name').val());
+            form.append('ville',    $('#edit-location').val());
+            form.append('avatar',   $('#edit-avatar').prop('files')[0]);
+            form.append('userId',   getUserId());
+
+            $.ajax({
+                url: DIESE_SERVICE+'edit/profile.php',
+                cache: false,
+                dataType: 'text',
+                contentType: false,
+                processData: false,
+                data: form,
+                type: 'post',
+                success: function(e){
+                    console.log(e);
+                    recupDataUser();
+                    closeSettings();
+                    closeLoading();
+                },
+                error: function(e){
+                    console.log(e);
+                    closeLoading();
+                }
+            });
+
+            return false;
+        });
+
         $('#edit-profile').animate({
             width: '100%',
             height: '100%',
@@ -62,60 +136,12 @@ function addEditListener(){
             width: '0',
             height: '0',
             opacity: 0
-        }, 'normal');
+        }, 'normal', function(){
+            $(this).remove();
+        });
     };
 
     $('#roue').on('click', function(){
         openSettings();
-    });
-
-    $('#cancel').on('click', function(){
-        closeSettings();
-    });
-
-    $('#edit-avatar').on('change', function(){
-        if (this.files && this.files[0]) {
-            var reader = new FileReader();
-
-            reader.onload = function (e) {
-                $('#edit-avatar-label').css({backgroundImage:'url('+ e.target.result+')'});
-            };
-
-            reader.readAsDataURL(this.files[0]);
-        }
-    });
-
-    $('#edit-form').on('submit', function(){
-        openLoading('Modification en cours');
-
-        var form = new FormData();
-
-        form.append('age',      $('#edit-age').val());
-        form.append('prenom',   $('#edit-name').val());
-        form.append('ville',    $('#edit-location').val());
-        form.append('avatar',   $('#edit-avatar').prop('files')[0]);
-        form.append('userId',   getUserId());
-
-        $.ajax({
-            url: DIESE_SERVICE+'edit/profile.php',
-            cache: false,
-            dataType: 'text',
-            contentType: false,
-            processData: false,
-            data: form,
-            type: 'post',
-            success: function(e){
-                console.log(e);
-                recupDataUser();
-                closeSettings();
-                closeLoading();
-            },
-            error: function(e){
-                console.log(e);
-                closeLoading();
-            }
-        });
-
-        return false;
     });
 }
