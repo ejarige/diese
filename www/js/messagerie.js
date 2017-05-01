80715aa581051a4f0d41d6214cecfc1eb208cde3
Jarrige <eric.jarrige@live.fr>
2017-05-01 18:10:36 +0200

    updated conversations and wip messages

--- www/js/messagerie.js
+++ www/js/messagerie.js
@@ -1,1 +1,28 @@
+$(function(){
+    getMessages();
 
+    addResponsive();
+});
+
+function getMessages(){
+    openLoading('Chargement des messages...');
+
+    var onLoad = function(e){
+        var data = $.parseJSON(data);
+
+        var mapDate = {};
+        for(var i in data){
+            var day = data[i].date - (data[i].date % 86400000);
+            // TODO
+        }
+    };
+
+    var onError = function(e){
+        console.log('Erreur '+e);
+    };
+}
+
+function addResponsive(){
+    var vh = $(window).height()/100;
+    $('#conversation').css('height', ($(window).height()-25*vh-49)+'px');
+}
