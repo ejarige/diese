2fa90c7ca24bf16b65db6fdd590afcf24e1b60a7
Xotia <gaillardk621@gmail.com>
2017-05-09 14:50:55 +0200

    concert.css : couleur des boutons
    concerts.js : image par défaut

--- www/js/concerts.js
+++ www/js/concerts.js
@@ -114,7 +114,7 @@ function searchConcerts(append){
 }
 
 function addItem(event){
-    var mulan = "http://static1.purebreak.com/articles/5/11/12/55/@/481104-mulan-aura-le-droit-a-son-film-en-live-a-200x200-1.jpg";
+    var mulan = "http://diese.pe.hu/avatar/default-image-concert.jpg";
     var img = event.image != null ? event.image.large.url : mulan;
     var item = '<div class="concert" id="'+event.id+'" style="background-image:url('+img+')">'
         + '<div class="info">'
